33dc6f68d020b64e3b90ac2717190f53ef0d9bc7
Eliott Lesimple <lesimpleliott@gmail.com>
2024-07-03 18:50:01 +0200

    Set default settings

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 // ***** SETTINGS FILE *****
 
 const outputFileExtension = "avif"; // Extension des fichiers de sortie
-const outputSize = 0.3; // Taille de l'image de sortie (0.1 à 1)
-const defaultAuthor = "test"; // Auteur par défaut
-const outputJSONFile = "expoooortDataaa"; // Nom du fichier JSON de sortie
+const outputSize = 0.5; // Taille de l'image de sortie (0.1 à 1)
+const defaultAuthor = "unsplash"; // Auteur par défaut
+const outputJSONFile = "exportData"; // Nom du fichier JSON de sortie
 
 module.exports = {
   outputFileExtension,
